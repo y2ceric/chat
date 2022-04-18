f78349088e34a733e46dddad137619011c1e351f
y2ceric <jieric1428@gmail.com>
2022-04-18 16:52:05 +0800

    update

--- type1.py
+++ type1.py
@@ -11,7 +11,7 @@ def read_file(address='input.txt'):
 	else:
 		print('找不到對話檔案')
 # 轉換資料
-def convert(chat, p1, p2, address='output.txt'):
+def convert(chat, p1, p2):
 	talker = None
 	new = []
 	for c in chat:
@@ -26,15 +26,16 @@ def convert(chat, p1, p2, address='output.txt'):
 	print('轉換完成')
 	return new
 # 存檔
-def write_file(output, address = 'output.txt'):
+def write_file(output, address='output.txt'):
 	with open(address, 'w', encoding = 'utf-8-sig') as f:
 		for line in output:
 			f.write(line + '\n')
 
 def main():
 	raw = read_file()
-	output = convert(raw, 'Allen', 'Tom')
-	write_file(output, 'test.txt')
+	if raw:
+		output = convert(raw, 'Allen', 'Tom')
+		write_file(output, 'result.txt')
 
 if __name__ == '__main__':
 	main()
